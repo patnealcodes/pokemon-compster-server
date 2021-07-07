ffd9b4e3f57f5331b9f924b46e8e70cf82254346
Patrick Neal <pat.neal@rocketinsights.com>
2021-07-07 11:51:15 -0400

    Troubleshooting heroku :3
    - Updated sever dependencies from dev*
    - Trying this silly "tsc": "tsc" thing  ¯\_(ツ)_/¯
    - Added a '/' route response for fun

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import * as dotenv from 'dotenv'
 import { teamController } from '../controllers'
 
@@ -6,6 +6,16 @@ dotenv.config()
 
 const router = Router()
 
+// Base Controller (TODO: Replace with API docs)
+router.get('/', (req: Request, res: Response) => {
+  try {
+    res.status(200).json('Hello!')
+  } catch (err: any) {
+    throw(err)
+  }
+})
+
+// Team Controllers
 router.post('/teams/create', teamController.create)
 router.get('/teams', teamController.getAllT)
 router.get('/teams/:id', teamController.get)
