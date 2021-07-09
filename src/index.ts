cfb25fc95baea940970660cdb403acd830354ccb
Patrick Neal <pat.neal@rocketinsights.com>
2021-07-09 09:28:13 -0400

    Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import { Server } from "./config"
 import { router } from "./routes"
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
